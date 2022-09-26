0defec58013164042285a1f3f3c4e30980fd643e
Pulkiiit <kpulkit30@gmail.com>
2022-09-26 10:20:12 +0530

    problem with db

--- app/__init__.py
+++ app/__init__.py
@@ -6,9 +6,12 @@ import os
 from run import arg
 
 DB_NAME = "DtBEsm4DXK"
+db = SQLAlchemy()
 key = arg
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = key
+    app.config['SQLALCHEMY_DATABASE_URI'] =
+    db.init_app(app)
